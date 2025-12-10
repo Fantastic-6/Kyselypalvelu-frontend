cd7a3c72989ad47085b9c631b984c609cf6bef74
iltar <ilta.riikonen@gmail.com>
2025-12-10 21:34:44 +0200

    attempt 3

--- src/components/SurveyView.tsx
+++ src/components/SurveyView.tsx
@@ -88,20 +88,22 @@ function SurveyView() {
   return (
     <>
       <h1>{survey?.title}</h1>
-      <form onSubmit={saveResponses}>
-        <a className="return-btn" href={"/https://fantastic-6.github.io/Kyselypalvelu-frontend"}>
+      <div className="container">
+        <a className="return-btn" href={"/"}>
           Takaisin etusivulle
         </a>
-        {questions.map((question) => (
-          <RenderQuestion
-            question={question}
-            sendDataToParent={handleDataFromChild}
-          />
-        ))}
-        <button className="green-btn" type="submit">
-          Lähetä vastaukset
-        </button>
-      </form>
+        <form onSubmit={saveResponses}>
+          {questions.map((question) => (
+            <RenderQuestion
+              question={question}
+              sendDataToParent={handleDataFromChild}
+            />
+          ))}
+          <button className="green-btn" type="submit">
+            Lähetä vastaukset
+          </button>
+        </form>
+      </div>
     </>
   );
 }
